1f9f22b25359d09732b081af36fedd266f18b4ed
Sathish Murugiah <Sathish.Murugiah@gmail.com>
2021-09-05 20:27:51 -0400

    SM-5

--- src/main/java/simpleFile.java
+++ src/main/java/simpleFile.java
@@ -4,6 +4,7 @@ public class simpleFile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Jenkins Docker Integration");
+System.out.println("Adding New Line 2");
 	}
 
 }
